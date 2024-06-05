e0088456d12a68254212f66d3af44a92068ba7f1
ignaciodz <ignaciodiazbr@gmail.com>
2024-06-05 02:26:18 -0500

    🔨 Feat: Agrega logica para el manejo de Drinks

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,15 +1,20 @@
 import { StateCreator } from "zustand"
-import { getCategories } from "../services/RecipeService"
-import type { Categories } from "../types"
+import { getCategories, getRecipes } from "../services/RecipeService"
+import type { Categories, Drink, Drinks, SearchFilter } from "../types"
 
 export type RecipesSliceType = {
     categories: Categories
+    drinks: Drinks
     fetchCategories: () => Promise<void>
-
+    searchRecipes: (searchFilter: SearchFilter) => Promise<void>
+    selectRecipe: (id: Drink['idDrink']) => Promise<void>
 }
 
 export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
     categories: {
+        drinks: []
+    },
+    drinks: {
         drinks: []
     },
     fetchCategories: async () => {
@@ -17,5 +22,14 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
         set({
             categories
         })
+    },
+    searchRecipes: async (filters) => {
+       const drinks = await getRecipes(filters)
+       set({
+            drinks
+       })
+    },
+    selectRecipe: async(id) => {
+        console.log(id)
     }
 })
