2546bc6f7f3069dd03bbb2cc679968ca93ec2281
ignaciodz <ignaciodiazbr@gmail.com>
2024-06-05 02:22:29 -0500

    🔨 Feat: Agrega logica para el filtrado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,40 @@
-import { useEffect, useMemo } from 'react'
+import { ChangeEvent, FormEvent, useEffect, useMemo, useState } from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 import { useAppStore } from '../stores/useAppStore'
 
 const Header = () => {
-  
+  const [searchFilters, setSearchFilters] = useState({
+    ingredient: '',
+    category: ''
+  })
   const { pathname } = useLocation()
   const isHome = useMemo(() => pathname === '/', [pathname])
 
   const fetchCategories = useAppStore((state) => state.fetchCategories)
   const categories = useAppStore((state) => state.categories)
+  const searchRecipes = useAppStore((state) => state.searchRecipes)
 
   useEffect(() => {
     fetchCategories()
   }, [])
   
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    setSearchFilters({
+      ...searchFilters, 
+      [e.target.name] : e.target.value
+    })
+  }
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      // TODO: Validar
+      if (Object.values(searchFilters).includes('')) {
+        console.log('Todos los campos son obligatorios')
+        return
+      }
+      //Consultar recetas
+      searchRecipes(searchFilters)
+  }
   return (
     <header className={isHome ? 'bg-header bg-center bg-cover': 'bg-slate-800'}>
       <div className="mx-auto container px-5 py-16">
@@ -40,6 +60,7 @@ const Header = () => {
         {isHome && (
           <form
             className='md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6'
+            onSubmit={handleSubmit}
           >
             <div className='space-y-4'>
               <label
@@ -52,6 +73,8 @@ const Header = () => {
                 name='ingredient'
                 className='p-3 w-full rounded-lg focus:outline-none'
                 placeholder='Nombre o Ingrediente. Ej. Vodka, Tequila, Café'
+                onChange={handleChange}
+                value={searchFilters.ingredient}
               />
             </div>
             <div className='space-y-4'>
@@ -60,9 +83,11 @@ const Header = () => {
                 className='block text-white uppercase font-extrabold text-lg'
               >Categoría</label>
               <select 
-                id='ingredient'
-                name='ingredient'
+                id='category'
+                name='category'
                 className='p-3 w-full rounded-lg focus:outline-none'
+                onChange={handleChange}
+                value={searchFilters.category}
               >
                 <option value=""> -- Seleccione -- </option>
                 {categories.drinks.map((category)=>(
