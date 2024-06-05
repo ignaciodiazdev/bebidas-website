ac22519763ada3b021eb16bd7b9ccf0e72a8c58d
ignaciodz <ignaciodiazbr@gmail.com>
2024-06-05 02:20:01 -0500

    🔨 Feat: Renderiza los datos de la API

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,8 +1,24 @@
+import { useMemo } from "react"
+import { useAppStore } from "../stores/useAppStore"
+import DrinkCard from "../components/DrinkCard"
 
 const IndexPage = () => {
+  const drinks = useAppStore(state => state.drinks)
+  const hasDrinks = useMemo(() => drinks.drinks.length,[drinks])
   return (
     <>
-      <div>IndexPage</div>
+      <h1 className="text-6xl font-extrabold">Recetas</h1>
+      { hasDrinks ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 my-10 gap-10">
+          {drinks.drinks.map((drink) => (
+            <DrinkCard key={drink.idDrink} drink={drink}/>
+          ))}
+        </div>
+      ) :  (
+        <p className="my-10 text-center text-2xl">
+          No hay resultado aún, utiliza el formulario para buscar recetas
+        </p>
+      )}
     </>
   )
 }
