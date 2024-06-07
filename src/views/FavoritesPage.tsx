3b50540bf1ded5afc3aebc5bdd80ace397566f8f
ignaciodz <ignaciodiazbr@gmail.com>
2024-06-07 17:21:33 -0500

    🔨 Feat: Renderizado de las Recetas Favoritas

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,7 +1,30 @@
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
 
 const FavoritesPage = () => {
+  const favorites = useAppStore(state => state.favorites)
+  const hasFavorites = useMemo(() => favorites.length ,[favorites])
+
+
   return (
-    <div>FavoritesPage</div>
+    <>
+      <h1 className="text-6xl  font-extrabold">Favoritos</h1>
+      {hasFavorites ? (
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 my-10 gap-10">
+            {favorites.map((drink) => (
+              <DrinkCard
+                key={drink.idDrink}
+                drink={drink}
+              />
+            ))}
+          </div>
+        </div>
+      ):(
+        <p className="my-40 text-center text-2xl">Los favoritos se mostrarán aquí!</p>
+      )}
+    </>
   )
 }
 
